5cfa2acde0e107bec442917ee211ff2cce4c7135
Alex <saxenaalex@gmail.com>
2024-11-18 13:20:57 +0100

    Adds Gender input capture & Validation call

--- VikingNameGeneratorApp/Program.cs
+++ VikingNameGeneratorApp/Program.cs
@@ -1,15 +1,27 @@
-﻿var userInputName = "";
+﻿var nameInput = "";
+var genderInput = "";
+
+var nameValidator = new ValidateName();
+var genderValidator = new ValidateGenderOptions();
 
 Console.WriteLine("Hello Viking to be! \nPlease answer these quick questions to get your new Viking name!");
 Console.WriteLine("1. What is your name?");
-userInputName = Console.ReadLine();
+nameInput = Console.ReadLine();
 
-var nameValidator = new ValidateName();
-
-while (!nameValidator.Validate(userInputName))
+while (!nameValidator.Validate(nameInput))
 {
     Console.WriteLine("Let's try again. What is your name?");
-    userInputName = Console.ReadLine();
+    nameInput = Console.ReadLine();
 }
 
-Console.WriteLine($"You wrote: {userInputName}");
+Console.WriteLine("2. What is your gender? (M/F/Random)");
+genderInput = Console.ReadLine();
+
+
+while (!genderValidator.Validate(genderInput))
+{
+    Console.WriteLine("Let's try again. What is your gender? (M/F/Random)");
+    genderInput = Console.ReadLine();
+}
+
+Console.WriteLine($"You wrote: {nameInput} & You selected: {genderInput}");
