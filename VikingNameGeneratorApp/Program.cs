786534ac18dca57d40d29877a5ad63d2ee0eefdd
Alex <saxenaalex@gmail.com>
2024-11-18 13:04:42 +0100

    Updates Program.cs to use validation call for name userInput

--- VikingNameGeneratorApp/Program.cs
+++ VikingNameGeneratorApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var userInputName = "";
+
+Console.WriteLine("Hello Viking to be! \nPlease answer these quick questions to get your new Viking name!");
+Console.WriteLine("1. What is your name?");
+userInputName = Console.ReadLine();
+
+var nameValidator = new ValidateName();
+
+while (!nameValidator.Validate(userInputName))
+{
+    Console.WriteLine("Let's try again. What is your name?");
+    userInputName = Console.ReadLine();
+}
+
+Console.WriteLine($"You wrote: {userInputName}");
