00a6afcf38254e9affb3ba066d992a520e1989f2
Alex <saxenaalex@gmail.com>
2024-11-18 13:43:21 +0100

    Adds title capture  & Validation call

--- VikingNameGeneratorApp/Program.cs
+++ VikingNameGeneratorApp/Program.cs
@@ -1,10 +1,13 @@
 ﻿var nameInput = "";
 var genderInput = "";
+var titleInput = "";
 
 var nameValidator = new ValidateName();
 var genderValidator = new ValidateGenderOptions();
+var titleValidator = new ValidateTitle();
 
 Console.WriteLine("Hello Viking to be! \nPlease answer these quick questions to get your new Viking name!");
+
 Console.WriteLine("1. What is your name?");
 nameInput = Console.ReadLine();
 
@@ -17,11 +20,20 @@ while (!nameValidator.Validate(nameInput))
 Console.WriteLine("2. What is your gender? (M or F)");
 genderInput = Console.ReadLine();
 
-
 while (!genderValidator.Validate(genderInput))
 {
     Console.WriteLine("Let's try again. What is your gender? (M or F)");
     genderInput = Console.ReadLine();
 }
 
-Console.WriteLine($"You wrote: {nameInput} & You selected: {genderInput}");
+Console.WriteLine("\n3. Choose your Viking destiny!");
+Console.WriteLine("Are you a mighty Warrior, a noble Royalty, or a humble Farmer?");
+titleInput = Console.ReadLine();
+
+while (!titleValidator.Validate(titleInput))
+{
+    Console.WriteLine("Let's try again! Please type Warrior, Royalty, or Farmer:");
+    titleInput = Console.ReadLine();
+}
+
+Console.WriteLine($"You wrote: {nameInput}, Gender: {genderInput}, Title: {titleInput}");
